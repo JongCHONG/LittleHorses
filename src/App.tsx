9841ad15e400f2102ae1048cf8b82c0a9c81291d
jochong27 <jochong27@gmail.com>
2025-08-10 09:43:41 +0200

    add new paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
 import "./App.css";
 import Board from "./components/Board";
+import { TanCirclePositions } from "./utils/TanPath";
+import { BurntSiennaCirclePositions } from "./utils/BurntSiennaPath";
+import { CambridgeBlueCirclePositions } from "./utils/CambridgeBluePath";
+import { PrussianBlueCirclePositions } from "./utils/PrussianBluePath";
 
 function App() {
-  const tanCirclePositions = [
-    { x: 0, y: 350 },
-    { x: 50, y: 350 },
-    { x: 100, y: 350 },
-    { x: 150, y: 350 },
-    { x: 200, y: 350 },
-    { x: 250, y: 350 },
-    { x: 300, y: 350 },
-    { x: 350, y: 350 },
-    { x: 350, y: 300 },
-    { x: 350, y: 250 },
-    { x: 350, y: 200 },
-    { x: 350, y: 150 },
-    { x: 350, y: 100 },
-    { x: 350, y: 50 },
-    { x: 350, y: 0 },
-    // ... Ajoute toutes les positions nécessaires
-  ];
   return (
     <>
-      <Board tanCirclePositions={tanCirclePositions} />
+      <Board
+        tanCirclePositions={TanCirclePositions}
+        burntSiennaCirclePositions={BurntSiennaCirclePositions}
+        cambridgeBlueCirclePositions={CambridgeBlueCirclePositions}
+        prussianBlueCirclePositions={PrussianBlueCirclePositions}
+      />
     </>
   );
 }
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Circle from "./Circle";
 
-const Board = ({ tanCirclePositions }: any) => {
+const Board = ({
+  tanCirclePositions,
+  burntSiennaCirclePositions,
+  cambridgeBlueCirclePositions,
+  prussianBlueCirclePositions,
+}: any) => {
   return (
     <div
       style={{
         position: "relative",
-        width: 800,
-        height: 800,
+        width: 850,
+        height: 850,
         background: "#fafaf0",
       }}
     >
@@ -19,7 +24,60 @@ const Board = ({ tanCirclePositions }: any) => {
           <Circle key={i} color="#DAB785" x={pos.x} y={pos.y} />
         )
       )}
-      {/* Recommence pour chaque zone de couleur (rouge, bleu, vert) en changeant la couleur ! */}
+
+      <div
+        style={{
+          width: 350,
+          height: 350,
+          background: "#C65D4D",
+          top: 0,
+          position: "absolute",
+          right: 0,
+        }}
+      >
+        <h2>Cercles Rouges</h2>
+      </div>
+      {burntSiennaCirclePositions.map(
+        (pos: { x: number; y: number }, i: React.Key | null | undefined) => (
+          <Circle key={i} color="#D5896F" x={pos.x} y={pos.y} />
+        )
+      )}
+
+      <div
+        style={{
+          width: 350,
+          height: 350,
+          background: "#70A288",
+          bottom: 0,
+          position: "absolute",
+          right: 0,
+        }}
+      >
+        <h2>Cercles Bleus</h2>
+      </div>
+      {cambridgeBlueCirclePositions.map(
+        (pos: { x: number; y: number }, i: React.Key | null | undefined) => (
+          <Circle key={i} color="#70A288" x={pos.x} y={pos.y} />
+        )
+      )}
+
+      <div
+        style={{
+          width: 350,
+          height: 350,
+          background: "#1969a1ff",
+          bottom: 0,
+          position: "absolute",
+          left: 0,
+        }}
+      >
+        <h2>Cercles Prussiens</h2>
+      </div>
+      {prussianBlueCirclePositions.map(
+        (pos: { x: number; y: number }, i: React.Key | null | undefined) => (
+          <Circle key={i} color="#1969a1ff" x={pos.x} y={pos.y} />
+        )
+      )}
     </div>
   );
 };
